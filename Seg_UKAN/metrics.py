d410ce9689f00f1de2d8dff17bb1081cd18ac27b
Spring317 <daoxuanquyhp@gmail.com>
2026-04-05 12:36:54 +0700

    Update: Remove old dependency. Upgrade to torch 2.11 for YOLO implementation

--- Seg_UKAN/metrics.py
+++ Seg_UKAN/metrics.py
@@ -2,9 +2,6 @@ import numpy as np
 import torch
 import torch.nn.functional as F
 
-from medpy.metric.binary import jc, dc, hd, hd95, recall, specificity, precision
-
-
 
 def iou_score(output, target):
     smooth = 1e-5
@@ -18,13 +15,13 @@ def iou_score(output, target):
     intersection = (output_ & target_).sum()
     union = (output_ | target_).sum()
     iou = (intersection + smooth) / (union + smooth)
-    dice = (2* iou) / (iou+1)
+    dice = (2 * iou) / (iou + 1)
 
     try:
         hd95_ = hd95(output_, target_)
     except:
         hd95_ = 0
-    
+
     return iou, dice, hd95_
 
 
@@ -35,8 +32,8 @@ def dice_coef(output, target):
     target = target.view(-1).data.cpu().numpy()
     intersection = (output * target).sum()
 
-    return (2. * intersection + smooth) / \
-        (output.sum() + target.sum() + smooth)
+    return (2.0 * intersection + smooth) / (output.sum() + target.sum() + smooth)
+
 
 def indicators(output, target):
     if torch.is_tensor(output):
@@ -46,12 +43,11 @@ def indicators(output, target):
     output_ = output > 0.5
     target_ = target > 0.5
 
-    iou_ = jc(output_, target_)
-    dice_ = dc(output_, target_)
-    hd_ = hd(output_, target_)
-    hd95_ = hd95(output_, target_)
-    recall_ = recall(output_, target_)
-    specificity_ = specificity(output_, target_)
-    precision_ = precision(output_, target_)
-
-    return iou_, dice_, hd_, hd95_, recall_, specificity_, precision_
+    # iou_ = jc(output_, target_)
+    # dice_ = dc(output_, target_)
+    # hd_ = hd(output_, target_)
+    # hd95_ = hd95(output_, target_)
+    # recall_ = recall(output_, target_)
+    # specificity_ = specificity(output_, target_)
+    # precision_ = precision(output_, target_)
+    #
